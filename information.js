1000dfefe12acb8b770dcfe47c253649e97d2433
marjoleine <marjegama@gmail.com>
2023-11-30 13:13:08 +0800

    2nd commit

---
+++ information.js
@@ -0,0 +1,101 @@
+export const datas = [
+    {
+      id:'1',
+      name: 'Marjoleine Egama',
+      nickName:'Jo',
+      address:'France',
+      courseAndYear:'BSIT-3',
+      avatar:
+        'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+    },
+    {
+        id:'2',
+        name: 'Rea Angela Galleros',
+        nickName:'Badowix',
+        address:'India',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'3',
+        name: 'Mae Flor Butawan',
+        nickName:'Buts',
+        address:'Sikatuna',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'4',
+        name: 'Jeevelyn Lunzon',
+        nickName:'Jev',
+        address:'Mars',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'5',
+        name: 'Rowena Suizo',
+        nickName:'Weng',
+        address:'Antartica',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'6',
+        name: 'Nikko Brylle Alturas',
+        nickName:'Nikkoy',
+        address:'Russia',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'7',
+        name: 'John Carlo Namor',
+        nickName:'Lo',
+        address:'Canada',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'8',
+        name: 'Dave Francis Baugbog',
+        nickName:'Dabi',
+        address:'London',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'9',
+        name: 'Karl Patrick Loquias',
+        nickName:'Karl',
+        address:'Venezuela',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'10',
+        name: 'Peter Kyle Gingo',
+        nickName:'Peter',
+        address:'Spain',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'11',
+        name: 'Maryjane Sanchez',
+        nickName:'Mj',
+        address:'Malaysia',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+  ];
